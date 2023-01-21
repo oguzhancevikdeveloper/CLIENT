5e5d75c3a5d18e7ad705287045376406de048be0
Oguzz <Oguzz@OGUZHAN_PC>
2023-01-22 00:03:22 +0300

    Ui notification toastr library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ declare var $: any
 })
 export class AppComponent {
   title = 'CLIENT';
+  constructor(private toastrService: CustomToastrService) {
+
+    toastrService.message("merhaba","angular",{
+       messageType: ToastrMessageType.Success,
+       position: ToastrPosition.TopCenter,
+    });
+    toastrService.message("merhaba","angular",{
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.BottomFullWidth
+    });
+  }
+
 }
